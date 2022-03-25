dbcd2775ab00d9007508b63cb5c243d0a89dd9b1
HM <hieumdd@gmail.com>
2022-03-25 13:23:58 +0700

    hotfix: Hotfix hapi controller

--- hapi/hapi_controller.py
+++ hapi/hapi_controller.py
@@ -5,5 +5,5 @@ from hapi.hapi_service import pipeline_service
 
 
 def hapi_controller(body: dict[str, Any]):
-    if body["resource"] in services:
-        return pipeline_service(services[body["resource"]])(body)
+    if body["table"] in services:
+        return pipeline_service(services[body["table"]])(body)
